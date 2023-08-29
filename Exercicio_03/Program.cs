2e3de45ccb8e00c14e6e7038b34844c42afdd821
Gustavo Vinicius Borges Rodrigues <gustavovinicimos12345@gmail.com>
2023-08-29 11:42:18 -0300

    feat : Resolvendo exercicio 3 da lista.

---
+++ Exercicio_03/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Digite a altura (em metros): ");
+double altura = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o sexo (M para masculino, F para feminino): ");
+char sexo = char.Parse(Console.ReadLine().ToUpper());
+
+double pesoIdeal = 0;
+
+if (sexo == 'M')
+{
+    pesoIdeal = (72.7 * altura) - 58;
+}
+else if (sexo == 'F')
+{
+    pesoIdeal = (62.1 * altura) - 44.7;
+}
+else
+{
+    Console.WriteLine("Sexo inválido.");
+    return;
+}
+
+Console.WriteLine($"O peso ideal é: {pesoIdeal:F2} kg");
